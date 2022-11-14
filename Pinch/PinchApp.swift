b339637fcd395bc20b291825bd6705f052b4e3cc
erika-h7 <erika.herrera1@outlook.com>
2022-11-14 08:51:29 -0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Infinity Code on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
