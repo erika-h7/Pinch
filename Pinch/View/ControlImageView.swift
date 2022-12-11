11ed31f644cb23b2b7ce5ac411521d9ea2244132
erika-h7 <erika.herrera1@outlook.com>
2022-12-10 19:14:30 -0800

    Created the Control Interface and functionality

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentImageView.swift
+//  Pinch
+//
+//  Created by Infinity Code on 12/10/22.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
